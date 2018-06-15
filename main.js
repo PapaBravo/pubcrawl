782730c81411223ae6f73812dbf3ee109cdc3aec
Igor Sekulic <igor.sekulic@cgi.com>
2018-06-15 20:35:38 +0200

    Bootstrap eingebunden und Styling angefangen

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-let map;
-let userPos;
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8
-    });
-    getUserLocation()
-        .then(pos => {
-            userPos = pos;
-            map.setCenter(pos);
-        });
+let map
+let userPos
+function initMap () {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
+  })
+  getUserLocation()
+    .then(pos => {
+      userPos = pos
+      map.setCenter(pos)
+    })
 }
 
-function getUserLocation() {
-    return new Promise((resolve, reject) => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(position => {
-                var pos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                };
-                resolve(pos);
-
-            }, reject);
-        } else {
-            console.warn("Geolocation doesn't work", err)
-            resolve({ lat: 53.30, lng: 10.0 });
+function getUserLocation () {
+  return new Promise((resolve, reject) => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
         }
-    });
+        resolve(pos)
+      }, reject)
+    } else {
+      console.warn("Geolocation doesn't work", err)
+      resolve({ lat: 53.30, lng: 10.0 })
+    }
+  })
 }
