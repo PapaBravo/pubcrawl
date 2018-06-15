c95574e0e5f0fe3ceef9326b3c83334cb3ae9d77
Paul Boeck <paul.boeck@cgi.com>
2018-06-15 18:32:28 +0200

    Add simple search

--- main.js
+++ main.js
@@ -9,7 +9,10 @@ function initMap() {
         .then(pos => {
             userPos = pos;
             map.setCenter(pos);
-        });
+        })
+        .then(() => {
+            getPubs();
+        })
 }
 
 function getUserLocation() {
@@ -26,6 +29,30 @@ function getUserLocation() {
         } else {
             console.warn("Geolocation doesn't work", err)
             resolve({ lat: 53.30, lng: 10.0 });
+        }
+    });
+}
+
+function getPubs() {
+    const request = {
+        query: '',
+        fields: ['name', 'rating', 'opening_hours', 'formatted_address', 'geometry'],
+        location: userPos,
+        radius: 1000,
+        type: 'bar',
+        openNow: true
+
+    }
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, (results, status) => {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                console.info(place);
+            }
+        } else {
+            console.warn('PlaceService Error', status, results);
         }
     });
 }
